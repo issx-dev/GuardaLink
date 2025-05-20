386f2574272d0cf5364f373390e458164cf4c4d4
fcorcar <fcorcar2006@g.educaand.es>
2025-05-20 18:41:33 +0200

    Login funcional

--- app.py
+++ app.py
@@ -1,38 +1,71 @@
 ##
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for
+from passlib.hash import pbkdf2_sha256
+
 
 app = Flask(__name__)
+app.secret_key = "clave_secreta"
 
 # Diccionario temporal para almacenar usuarios
-usuarios = {}
+usuarios = [] # TODO BD
 
 ##
 @app.route("/")
 def index():
-    return redirect("acceso")
-    return render_template("index.html")
+    user = session.get("usuario")
+
+    # Si hay un usuario logueado
+    if user:
+        return render_template("index.html")
+
+    # Si NO hay un usuario logueado
+    return redirect(url_for("acceso"))
 
 
 ##
 @app.route("/acceso", methods=["GET", "POST"])
 def acceso():
     if request.method == "POST":
+        # Obtiene los datos del formulario html
         nombre_completo = request.form.get("nombre-completo")
         email = request.form.get("email")
         contraseña = request.form.get("contraseña")
 
+        # Registro
         if "registrarse" in request.form:
-            print(nombre_completo, email, contraseña)
-            print("Registro")
+            for usuario in usuarios:
+                if usuario["email"] == email: # TODO BD
+                    return "Este email ya esta registrado"
+                
+            else:
+                usuarios.append({ # TODO BD
+                    "nombre_completo": nombre_completo,
+                    "email": email,
+                    "contraseña": pbkdf2_sha256.hash(contraseña) # Encripta la pass al subirla
+                })
 
+                session["usuario"] = email
+                return redirect(url_for("index"))
 
+        #Inicio de sesión
         elif "iniciar-sesion" in request.form:
-            print(email, contraseña)
-            print("Inicio de sesion")
-
-        
+            for usuario in usuarios:
+                if usuario["email"] == email and pbkdf2_sha256.verify(contraseña, usuario["contraseña"]): # TODO BD
+                    session["usuario"] = email
+                    return redirect(url_for("index"))
+                
+            else:
+                return "Email o contraseña incorrecta"
 
     return render_template("login.html")
+
+
+##
+@app.route("/cerrar-sesion")
+def cerrar_sesion():
+    session.pop("usuario", None)
+    return redirect(url_for("index"))
+
 
 
 
