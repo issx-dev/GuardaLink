64de55f3dd17f98dc09f038b726f478581887835
fcorcar <fcorcar2006@g.educaand.es>
2025-05-20 13:53:55 +0200

    Endpoint inicio de sesióny registro

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 ##
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -7,7 +7,28 @@ app = Flask(__name__)
 ##
 @app.route("/")
 def index():
+    return redirect("acceso")
     return render_template("index.html")
+
+
+##
+@app.route("/acceso", methods=["GET", "POST"])
+def acceso():
+    if request.method == "POST":
+        nombre_completo = request.form["nombre-completo"]
+        email = request.form["email"]
+        contraseña = request.form["contraseña"]
+
+        if "registrarse" in request.form:
+            print("Registro")
+
+
+        elif "iniciar-sesion" in request.form:
+            print("Inicio de sesion")
+
+        
+
+    return render_template("login.html")
 
 
 
