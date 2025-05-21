14a75686b1e280bd4e8b32aecbb5def2bfd9e551
fcorcar <fcorcar2006@g.educaand.es>
2025-05-21 18:12:24 +0200

    Conexión de Flask con BD

--- app.py
+++ app.py
@@ -3,27 +3,31 @@ from flask import Flask, render_template, request, redirect, session, url_for, f
 from passlib.hash import pbkdf2_sha256
 from settings import SECRET_KEY
 from db.utils.consultas import obtener_rol
+from db.BaseDatos import gestor_bd
+from db.models.Usuario import UsuarioInsert, UsuarioBD, Usuario
+from settings import INSERTAR_USUARIO, CONSULTA_USUARIO, CONSULTA_USUARIO_COMPLETO
 
 
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
-# Diccionario temporal para almacenar usuarios
-usuarios = []  # TODO BD
-
 
 ##
 @app.route("/")
 def index():
+    # Obtiene el usuario logueado y su rol
     user = session.get("usuario")
+    rol_usuario = obtener_rol(user)
 
-    rol_usuario = obtener_rol(user) if user else None
-
-    # Si hay un usuario logueado
-    if rol_usuario is None:
+    # Si NO hay un usuario logueado
+    if rol_usuario == None:
         return redirect(url_for("acceso"))
+    
+    # Si el usuario logueado es ADMIN
     elif rol_usuario == "admin":
         return "Eres admin"
+    
+    # Si el usuario logueado es NORMAL
     else:
         return render_template("index.html")
 
@@ -37,22 +41,27 @@ def acceso():
         email = request.form.get("email")
         contraseña = request.form.get("contraseña")
 
+        # Recuperción de los usuarios como instancias de la clase UsuarioBD
+        consulta_completa_resultado = gestor_bd.ejecutar_consulta(CONSULTA_USUARIO_COMPLETO)
+        if isinstance(consulta_completa_resultado, list):
+            usuarios = [UsuarioBD(*usr) for usr in consulta_completa_resultado]
+        else:
+            usuarios = []
+        
+
         # Registro
         if "registrarse" in request.form:
             for usuario in usuarios:
-                if usuario["email"] == email:  # TODO BD
-                    flash("Este email ya está registrado, inicie sesión", "error")
+                if usuario.email == email:
+                    flash("Este email ya está registrado, por favor Inicie Sesión.", "error")
                     return redirect(url_for("acceso"))
 
             else:
-                usuarios.append(
-                    {  # TODO BD
-                        "nombre_completo": nombre_completo,
-                        "email": email,
-                        "contraseña": pbkdf2_sha256.hash(
-                            contraseña
-                        ),  # Encripta la pass al subirla
-                    }
+                # Inserción de usuario en la BD
+                usr = UsuarioInsert(nombre_completo, pbkdf2_sha256.hash(contraseña), email)
+                gestor_bd.ejecutar_consulta(
+                    INSERTAR_USUARIO,
+                    usr.obtener_datos,
                 )
 
                 session["usuario"] = email
@@ -61,9 +70,9 @@ def acceso():
         # Inicio de sesión
         elif "iniciar-sesion" in request.form:
             for usuario in usuarios:
-                if usuario["email"] == email and pbkdf2_sha256.verify(
-                    contraseña, usuario["contraseña"]
-                ):  # TODO BD
+                if usuario.email == email and pbkdf2_sha256.verify(
+                    contraseña, usuario.contraseña
+                ):
                     session["usuario"] = email
                     return redirect(url_for("index"))
 
