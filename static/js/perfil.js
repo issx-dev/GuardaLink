ad740297508cf3995c2513d94f3ead1d348d0c37
fcorcar <fcorcar2006@g.educaand.es>
2025-05-24 12:44:07 +0200

    perfil html, css y js con estructura completada

--- static/js/perfil.js
+++ static/js/perfil.js
@@ -1,17 +1,19 @@
-var messageBox = document.querySelector('.js-message');
-  var btn = document.querySelector('.js-message-btn');
-  var card = document.querySelector('.js-profile-card');
-  var closeBtn = document.querySelectorAll('.js-message-close');
 
-  btn.addEventListener('click',function (e) {
-      e.preventDefault();
-      card.classList.add('active');
-  });
+const popup = document.getElementById("popupContrasena");
+const abrirBtn = document.getElementById("abrirPopup");
+const cancelarBtn = document.getElementById("btnCancelar");
 
-  closeBtn.forEach(function (element, index) {
-     console.log(element);
-      element.addEventListener('click',function (e) {
-          e.preventDefault();
-          card.classList.remove('active');
-      });
-  });
+abrirBtn.addEventListener("click", () => {
+    popup.style.display = "flex";
+});
+
+cancelarBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === popup) {
+        popup.style.display = "none";
+    }
+});
+
