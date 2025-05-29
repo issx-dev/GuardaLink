673db5a0100fde1f0d6834177fd299548f8cf793
Moha <mkadbri180@g.educaand.es>
2025-05-29 14:23:40 +0200

    Consulta actualizar rol de usuario

--- db/queries/usuarios.py
+++ db/queries/usuarios.py
@@ -14,5 +14,8 @@ ACTUALIZAR_USUARIO = " UPDATE usuarios SET nombre_completo = ?, foto_perfil = ?,
 # Modificar Estado de Usuario
 MODIFICAR_ESTADO_USUARIO = "UPDATE usuarios SET estado = ? WHERE id = ?"
 
+# Actualizar Rol de Usuario
+ACTUALIZAR_ROL_USUARIO = "UPDATE usuarios SET rol = ? WHERE id = ?"
+
 # Eliminar Usuario
 BORRAR_USUARIO = "DELETE FROM usuarios WHERE id = ?"
