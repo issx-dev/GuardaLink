d990ecb8d07c8b8f8a224c22700f4ac70f4b8dba
Moha <mkadbri180@g.educaand.es>
2025-05-29 14:17:21 +0200

    Consulta para buscar todos los usuarios

--- db/queries/usuarios.py
+++ db/queries/usuarios.py
@@ -4,7 +4,7 @@
 INSERTAR_USUARIO = "INSERT INTO usuarios (nombre_completo, contraseña, email) VALUES (?, ?, ?)"
 
 # Consultas de Usuario
-CONSULTA_USUARIO = "SELECT id, nombre_completo, rol, email, estado, foto_perfil, fecha_registro FROM usuarios"
+CONSULTA_USUARIOS = "SELECT * FROM usuarios"
 CONSULTA_USUARIO_COMPLETO = "SELECT * FROM usuarios WHERE email = ? "
 BUSCAR_USUARIO = "SELECT * FROM usuarios WHERE id = ?"
 
@@ -13,9 +13,6 @@ ACTUALIZAR_USUARIO = " UPDATE usuarios SET nombre_completo = ?, foto_perfil = ?,
 
 # Modificar Estado de Usuario
 MODIFICAR_ESTADO_USUARIO = "UPDATE usuarios SET estado = ? WHERE id = ?"
-
-# Actualizar Rol de Usuario
-ACTUALIZAR_ROL_USUARIO = "UPDATE usuarios SET rol = ? WHERE id = ?"
 
 # Eliminar Usuario
 BORRAR_USUARIO = "DELETE FROM usuarios WHERE id = ?"
