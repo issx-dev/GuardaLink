d7bc420ab95b35961d4f6e7d642820913d65c521
Moha <mkadbri180@g.educaand.es>
2025-05-29 10:56:55 +0200

    Añadido script sql para buscar y borrar usuario

--- db/queries/usuarios.py
+++ db/queries/usuarios.py
@@ -6,6 +6,10 @@ INSERTAR_USUARIO = "INSERT INTO usuarios (nombre_completo, contraseña, email) V
 # Consultas de Usuario
 CONSULTA_USUARIO = "SELECT id, nombre_completo, rol, email, estado, foto_perfil, fecha_registro FROM usuarios"
 CONSULTA_USUARIO_COMPLETO = "SELECT * FROM usuarios WHERE email = ? "
+BUSCAR_USUARIO = "SELECT * FROM usuarios WHERE id = ?"
 
 # Actualizar Usuario
 ACTUALIZAR_USUARIO = " UPDATE usuarios SET nombre_completo = ?, foto_perfil = ?, email = ?, contraseña = ? WHERE id = ? "
+
+# Eliminar Usuario
+BORRAR_USUARIO = "DELETE FROM usuarios WHERE id = ?"
